31b405e222bdd772d71fcc8c8c7883efabce5bc9
Mike <michael.w.magnoli@gmail.com>
2018-07-24 16:02:47 -0400

    feat: add dependency version helper

---
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -0,0 +1,13 @@
+package com.mikemagss.kotlingradlebuildscriptsexercise
+
+object DependencyVersions {
+    // General dependencies
+    const val Kotlin = "1.2.21"
+    const val SupportLibraries = "27.1.0"
+
+    // Test dependencies
+    const val Junit = "4.12"
+    const val AndroidTestRunner = "1.0.1"
+    const val Espresso = "3.0.1"
+    const val ConstraintLayout = "1.1.2"
+}
